2f7c00b2d7678a89cbd7940379ab8c93246d4da0
khaitn1309 <35931018+khaitn1309@users.noreply.github.com>
2018-06-07 11:37:08 +0700

    Update worker.py

--- worker.py
+++ worker.py
@@ -397,7 +397,7 @@ class WorkerBot(BaseWorkerBot):
 
 def get_parser():
     parser = OptionParser(usage='%prog [options]')
-    parser.add_option('--server', '-s', dest='server', default='irc.freenode.net',
+    parser.add_option('--server', '-s', dest='server', default='irc.atmmtnc.tk',
         help='IRC server to connect to')
     parser.add_option('--port', '-p', dest='port', default=6667,
         help='Port to connect on', type='int')
